90f55eef804a036d215ed99c0fccc53aa3b719a0
Julien Baudru <julien.baudru@gmail.com>
2021-12-07 10:50:11 +0100

    data type

--- main.jl
+++ main.jl
@@ -43,21 +43,19 @@ end
 
 # ==============================================================================
 function lasVegas(formula, nb_clause, nb_var)
-    # Assign random truth value to the variable
     varvalue = Dict()
-    for i = 1:nb_var
+    for i = 1:nb_var # Assign random truth value to the variable
         varvalue[i] = rand((0, 1))
     end
-
-    nb_satisf_clause = 0
+    nb_satisf_clause::Int32 = 0
     for i = 1:nb_clause
-        var = varvalue[abs(formula[i, 1])]
+        var::Int32 = varvalue[abs(formula[i, 1])]
         if (formula[i,1]< 0)
             if (var == 0) var = 1
             else var = 0
             end
         end
-        isClauseSatisfied = var
+        isClauseSatisfied::Int32 = var
         for k in 2:3
             var = varvalue[abs(formula[i, k])]
             if (formula[i,k]< 0)
@@ -71,13 +69,12 @@ function lasVegas(formula, nb_clause, nb_var)
             nb_satisf_clause += 1
         end
     end
-    #println("Clauses satisf. : ", nb_satisf_clause, "/91")
     return nb_satisf_clause/nb_clause
 end
 
 function lasVegasRunner(formula, nb_clause, nb_var)
-    nb_try = 1
-    success = lasVegas(formula, nb_clause, nb_var)
+    nb_try::Int32 = 1
+    success::Float32 = lasVegas(formula, nb_clause, nb_var)
     while (success < 7/8)
         success = lasVegas(formula, nb_clause, nb_var)
         nb_try += 1
@@ -87,30 +84,21 @@ end
 
 # ==============================================================================
 function main()
-    # For each file in the folder
     folder_dic = Dict{String, Integer}("data/0uf20-91/uf20-0" => 1000, "data/1uf50-218/uf50-0" => 1000, "data/2uf75-325/uf75-0" => 100, "data/3uf100-430/uf100-0" => 1000, "data/4uf125-538/uf125-0" => 100, "data/5uf150-645/uf150-0" => 100, "data/6uf175-753/uf175-0" => 100, "data/7uf200-860/uf200-0" => 100, "data/8uf225-960/uf225-0" => 100, "data/9uf250-1065/uf250-0" => 100)
     folder_dic = sort(collect(folder_dic))
-
     x = [20, 50, 75, 100, 125, 150, 175, 200, 225, 250]
     xx = [91, 218, 325, 430, 538, 645, 753, 860, 960, 1065]
-    y = zeros(0) # time
-    z = zeros(0) # success
-    w = zeros(0) # tries
-    cmpt = 1
+    y = zeros(0); z = zeros(0); w = zeros(0) # tries
+    cmpt::Int32 = 1
     for (filename_tmp, nb_file) in folder_dic
         println("Data : ", filename_tmp)
-        print("Nb. variables : ")
-        printstyled(x[cmpt]; color = :yellow)
-        print("\nNb. clauses : ")
-        printstyled(xx[cmpt]; color = :yellow)
+        print("Nb. variables : "); printstyled(x[cmpt]; color = :yellow)
+        print("\nNb. clauses : "); printstyled(xx[cmpt]; color = :yellow)
         cmpt+=1
-        avg_time = 0
-        avg_success = 0
-        avg_try = 0
+        avg_time::Float32 = 0; avg_success::Float32 = 0; avg_try::Float32 = 0
         for i=1:nb_file # For each file in the folder
             filename = filename_tmp * string(i) * ".cnf"
-            formula, nb_clause, nb_var = getData(filename)
-            #readableCnf(formula)
+            formula, nb_clause::Int32, nb_var::Int32 = getData(filename)
             avg_time += @elapsed lasVegasRunner(formula, nb_clause, nb_var)
             tmp_success, tmp_try = lasVegasRunner(formula, nb_clause, nb_var)
             avg_success += tmp_success
@@ -120,19 +108,14 @@ function main()
         append!(z, avg_success/nb_file)
         append!(w, avg_try/nb_file)
         println("\nResults :")
-        printstyled("   Avg. time : "; color = :green)
-        print(avg_time/nb_file)
-        printstyled("\n   Avg. success : ", color = :green)
-        print(avg_success/nb_file)
-        printstyled("\n   Avg. try : ", color = :green)
-        print(avg_try/nb_file)
+        printstyled("   Avg. time : "; color = :green); print(avg_time/nb_file)
+        printstyled("\n   Avg. success : ", color = :green); print(avg_success/nb_file)
+        printstyled("\n   Avg. try : ", color = :green); print(avg_try/nb_file)
         println("\n")
     end
     # TODO : Axes titles
     # TODO : Correct tick axes
     # TODO : Add plot to report
-    # TODO : Make general average
-    # TODO : Change all data type
     plot(x, z, title = "Average success per number of variable in 3SAT")
     savefig("img/results1.png")
     plot(x, y, title = "Average running time per number of variable in 3SAT")
